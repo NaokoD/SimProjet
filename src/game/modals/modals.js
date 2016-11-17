2445c1dbfd98f52afaa1d2c1f4b7b13f40609665
ludo <dbludo@live.fr>
2016-11-17 10:20:57 +0100

    Ajout Slider

--- src/game/modals/modals.js
+++ src/game/modals/modals.js
@@ -26,19 +26,28 @@ class ModalsController {
       //   this.status = 'You cancelled the dialog.';
       // });
     };
+  }
+}
 
-    function DialogController($scope, $mdDialog, categorie) {
-      $scope.categorie = categorie;
-      $scope.hide = function () {
-        $mdDialog.hide();
-      };
-      $scope.cancel = function () {
-        $mdDialog.cancel();
-      };
-      $scope.answer = function (answer) {
-        $mdDialog.hide(answer);
-      };
-    }
+class DialogController {
+  constructor($scope, $mdDialog, categorie) {
+    $scope.categorie = categorie;
+    $scope.budget = 14000;
+    // $http.get('/categories').then(function(response){
+    //   this.categorie=response.data.categories;
+    // });
+    $scope.categories = [{libelle: 'Education', budget: 50},
+                          {libelle: 'Sante', budget: 20},
+                          {libelle: 'Securite', budget: 20}];
+    $scope.hide = function () {
+      $mdDialog.hide();
+    };
+    $scope.cancel = function () {
+      $mdDialog.cancel();
+    };
+    $scope.answer = function (answer) {
+      $mdDialog.hide(answer);
+    };
   }
 }
 
