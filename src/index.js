7534ede0ff5e452465b119b843658c2a31395065
ludo <dbludo@live.fr>
2016-11-17 13:32:15 +0100

    Modif

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 angular
-<<<<<<< HEAD
-  .module('app', ['ui.router', 'ngMaterial']);
-=======
-<<<<<<< HEAD
   .module('app', ['ui.router', 'ngMaterial','chart.js']);
-=======
-  .module('app', ['ui.router', 'ngMaterial', 'ngMessages']);
->>>>>>> refs/remotes/origin/master
->>>>>>> 70bc8639a27cfc0764195be2cbf97ec0840b2903
